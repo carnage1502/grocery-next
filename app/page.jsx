8a883f368daf9a0cbd87bc03e8bec30752e6d7ab
carnage1502 <shivamsanjay152@gmail.com>
2024-03-27 23:51:49 +0530

    install nextjs, tailwindcss, shadcn, react-icons and create first template

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2>Hello Moto</h2>
+      <Button>oh yeah</Button>
+    </div>
+  );
+};
+
+export default page;
